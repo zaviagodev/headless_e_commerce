38e37ddc11c33c952b4d3d99a96f48aa53d2cee7
umer2001 <umer2001.uf@gmail.com>
2023-09-08 08:10:21 +0500

    fix: installation Error

--- headless_e_commerce/install_events.py
+++ headless_e_commerce/install_events.py
@@ -6,7 +6,6 @@ def after_install():
         "show_price": 1,
         "show_stock_availability": 1,
         "allow_items_not_in_stock": 1,
-        "price_list": "Standard Selling",
         "enabled": 1,
 
     }).save()
