a806ac6b46d87eb1be5c476ea89f6b098a1a40e3
umer2001 <umer2001.uf@gmail.com>
2023-12-24 22:30:19 +0500

    add: default required settings

--- headless_e_commerce/install_events.py
+++ headless_e_commerce/install_events.py
@@ -7,5 +7,8 @@ def after_install():
         "show_stock_availability": 1,
         "allow_items_not_in_stock": 1,
         "enabled": 1,
+        "price_list": "Standard Selling",
+        "quotation_series": "SAL-QTN-.YYYY.-",
+        "default_customer_group": "All Customer Groups",
 
     }).save()
